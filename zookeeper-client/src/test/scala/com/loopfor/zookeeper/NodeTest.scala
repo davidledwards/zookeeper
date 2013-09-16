8bb6cbbbfef4b753caaca13d9f9912b357c8aafe
David Edwards <dedwards@cerner.com>
2013-09-16 17:17:21 -0500

    fix erroneous unit test

--- zookeeper-client/src/test/scala/com/loopfor/zookeeper/NodeTest.scala
+++ zookeeper-client/src/test/scala/com/loopfor/zookeeper/NodeTest.scala
@@ -70,13 +70,13 @@ class NodeTest extends ZookeeperSuite {
   }
 
   test("create persistent node") { root =>
-    val path = root + "foo"
+    val path = root resolve "foo"
     val node = Node(path).create(Array(), ACL.AnyoneAll, Persistent)
     assert(node.path === path)
   }
 
   test("set and get") { root =>
-    val node = Node(root + "foo").create(Array(), ACL.AnyoneAll, Persistent)
+    val node = Node(root resolve "foo").create(Array(), ACL.AnyoneAll, Persistent)
     val in = randomBytes()
     node.set(in, Some(0))
     val (out, status) = node.get()
@@ -84,7 +84,7 @@ class NodeTest extends ZookeeperSuite {
   }
 
   test("set and get without version") { root =>
-    val node = Node(root + "foo").create(Array(), ACL.AnyoneAll, Persistent)
+    val node = Node(root resolve "foo").create(Array(), ACL.AnyoneAll, Persistent)
     val in = randomBytes()
     node.set(in, None)
     val (out, status) = node.get()
@@ -92,19 +92,19 @@ class NodeTest extends ZookeeperSuite {
   }
 
   test("set with wrong version") { root =>
-    val node = Node(root + "foo").create(Array(), ACL.AnyoneAll, Persistent)
+    val node = Node(root resolve "foo").create(Array(), ACL.AnyoneAll, Persistent)
     intercept[BadVersionException] {
       node.set(randomBytes(), Some(Int.MaxValue))
     }
   }
 
   test("node exists") { root =>
-    val node = Node(root + "foo").create(Array(), ACL.AnyoneAll, Persistent)
+    val node = Node(root resolve "foo").create(Array(), ACL.AnyoneAll, Persistent)
     assert(node.exists().isDefined)
   }
 
   test("node does not exist") { root =>
-    assert(Node(root + "foo").exists().isEmpty)
+    assert(Node(root resolve "foo").exists().isEmpty)
   }
 
   private def randomBytes() = UUID.randomUUID().toString.getBytes("UTF-8")
