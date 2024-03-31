fa8aab7e5f100e3880d430e1546666493ed8487a
David Edwards <david.l.edwards@gmail.com>
2024-03-31 16:23:50 -0500

    add API documentation for Observer

--- zookeeper-client/src/main/scala/com/loopfor/zookeeper/Observer.scala
+++ zookeeper-client/src/main/scala/com/loopfor/zookeeper/Observer.scala
@@ -17,30 +17,53 @@ package com.loopfor.zookeeper
 
 import org.apache.zookeeper.Watcher.WatcherType
 
+/**
+ * Specifies the type of watch that can be associated with a node.
+ */
 sealed trait Observer {
   private[zookeeper] val kind: WatcherType
 }
 
+/**
+ * Indicates any type of watch.
+ */
 object AnyObserver extends Observer {
   override def toString: String = "Any"
   private[zookeeper] val kind = WatcherType.Any
 }
 
+/**
+ * Indicates a watch for changes in children.
+ */
 object ChildrenObserver extends Observer {
   override def toString: String = "Children"
   private[zookeeper] val kind = WatcherType.Children
 }
 
+/**
+ * Indicates a watch for changes in data.
+ */
 object DataObserver extends Observer {
   override def toString: String = "Data"
   private[zookeeper] val kind = WatcherType.Data
 }
 
+/**
+ * Indicates a persistent watch for changes in children and data.
+ *
+ * A persistent watch is not automatically removed when triggered.
+ */
 object PersistentObserver extends Observer {
   override def toString: String = "Persistent"
   private[zookeeper] val kind = WatcherType.Persistent
 }
 
+/**
+ * Indicates a persistent and recursive watch for changes in children and data.
+ *
+ * A persistent watch is not automatically removed from triggered. The recursive
+ * nature means that the watch applies to all child nodes.
+ */
 object PersistentRecursiveObserver extends Observer {
   override def toString: String = "PersistentRecursive"
   private[zookeeper] val kind = WatcherType.PersistentRecursive
